b83f5ed434cff14d446bb4826b27b5e528b0efe8
bp4h <al.logvinov@gmail.com>
2023-11-08 17:16:38 +0100

    Add project files.

---
+++ ImageToWebPConverter/wwwroot/saveAsFile.js
@@ -0,0 +1,8 @@
+﻿window.saveAsFile = function (filename, dataBase64) {
+    var link = document.createElement("a");
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + dataBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
